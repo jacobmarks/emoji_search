a6bd9554943e432f77caa84db3098db7a90a4e9d
Jacob Marks <jamarks13@gmail.com>
2023-11-04 18:41:52 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Emoji Search
+
+Emoji Search is a Python-based CLI (Command Line Interface) application that allows users to semantically search emojis.
+
+## Installation
+
+You can use the package manager [pip](https://pip.pypa.io/en/stable/) to install emoji_search.
+
+```bash
+pip install emoji_search
+```
+
+Or you can install it from source:
+
+````bash
+pip install git+https://github.com/jacobmarks/emoji_search.git
+
+## Usage
+
+From the command line, use the `emoji-search` command, followed by a search term.
+
+```bash
+emoji-search beautiful sunset
+````
+
+This will return a list of emojis that most closely match the search term:
+
+```bash
++-------+------------------------+---------+----------+
+| Emoji |          Name          | Unicode | Distance |
++-------+------------------------+---------+----------+
+|  🌇   |         sunset         | U+1F307 |  0.025   |
+|  🌅   |        sunrise         | U+1F305 |  0.051   |
+|  🌄   | sunrise over mountains | U+1F304 |  0.111   |
+|  🌆   |   cityscape at dusk    | U+1F306 |  0.152   |
+|   ☀   |          sun           | U+2600  |  0.165   |
++-------+------------------------+---------+----------+
+```
+
+You can use quotation marks around your search term if you would like, but it is not necessary.
+
+### Flags
+
+You can specify the number of results you would like to see by using the `-n` or `--num_results` flag.
+
+```bash
+
+emoji-search -n 3 sleepy
+```
+
+This will return the top 3 results:
+
+```bash
++-------+---------------+---------+----------+
+| Emoji |     Name      | Unicode | Distance |
++-------+---------------+---------+----------+
+|  💤   |      zzz      | U+1F4A4 |  0.042   |
+|  😪   |  sleepy face  | U+1F62A |  0.042   |
+|  😴   | sleeping face | U+1F634 |  0.071   |
++-------+---------------+---------+----------+
+```
+
+## How it Works
+
+Emoji Search is a semantic search engine using the [CLIP](https://github.com/openai/CLIP)
+model from OpenAI. CLIP is a neural network trained on a variety of image-text pairs, and
+is able to semantically match images and text.
+
+To match emojis, Emoji Search attempts three different methods:
+
+1. Compare the embedding for the raw search term to the embeddings for all names
+   of emojis.
+2. Compare the embedding of the formatted search query
+   "An emoji of <search-query>" to the embeddings of similarly formatted emoji
+   names.
+3. Compare the embedding of the formatted search query with the embeddings of
+   high-resolution images of emojis.
+
+The results from each of these methods are combined and sorted by distance to
+the search query. The distance is calculated using cosine similarity.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ emoji_search/main.py
@@ -0,0 +1,188 @@
+# import json
+import pickle
+import gdown
+from tabulate import tabulate
+from appdirs import user_data_dir
+
+import argparse
+import os
+
+import numpy as np
+from scipy.spatial.distance import cosine
+import torch
+import clip
+
+PACKAGE_NAME = "emoji_search"
+
+DATA_DIR = user_data_dir(PACKAGE_NAME)
+EMBEDDINGS_FILE_NAME = "emoji_embeddings.pkl"
+
+EMBEDDINGS_PATH = os.path.join(DATA_DIR, EMBEDDINGS_FILE_NAME)
+
+
+import requests
+
+
+def download_embeddings():
+    if not os.path.isfile(EMBEDDINGS_PATH):
+        # If the file doesn't exist, download it
+        print(f"Downloading embeddings to {EMBEDDINGS_PATH}...")
+        os.makedirs(DATA_DIR, exist_ok=True)  # Ensure the DATA_DIR exists
+
+        # Download the file
+        file_id = "1JYM1lhgBzD9C34V-BICTzEhbW-JI9rPP"
+        URL = "https://drive.google.com/uc?export=download"
+        session = requests.Session()
+        response = session.get(URL, params={"id": file_id}, stream=True)
+        token = get_confirm_token(response)
+
+        if token:
+            params = {"id": file_id, "confirm": token}
+            response = session.get(URL, params=params, stream=True)
+
+        CHUNK_SIZE = 32768
+        with open(EMBEDDINGS_PATH, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+        print("Download complete.")
+    else:
+        print("Embeddings already downloaded.")
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+    return None
+
+
+def load_embeddings():
+    # Ensure the embeddings are downloaded
+    download_embeddings()
+
+    # Load the embeddings
+    embeddings = pickle.load(open(EMBEDDINGS_PATH, "rb"))
+    return embeddings
+
+
+def embed_query(query):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, _ = clip.load("ViT-B/32", device)
+
+    text = clip.tokenize([query]).to(device)
+    with torch.no_grad():
+        text_features = model.encode_text(text)
+
+    return text_features.detach().cpu().numpy()[0]
+
+
+def search(query, top_n=5):
+    embeddings = load_embeddings()
+
+    raw_query = query.lower()
+    emoji_of_text_query = f"An emoji of {raw_query}"
+
+    raw_query_embedding = embed_query(raw_query)
+    emoji_of_text_query_embedding = embed_query(emoji_of_text_query)
+
+    # distances = []
+    raw_text_distances = []
+    emoji_of_text_distances = []
+    image_distances = []
+
+    for name, props in embeddings.items():
+        raw_text_embedding = props["raw_text_embedding"]
+        emoji_of_text_embedding = props["emoji_of_text_embedding"]
+        image_embedding = props["image_embedding"]
+
+        raw_text_distance = cosine(raw_query_embedding, raw_text_embedding)
+        emoji_of_text_distance = cosine(
+            emoji_of_text_query_embedding, emoji_of_text_embedding
+        )
+        image_distance = cosine(emoji_of_text_query_embedding, image_embedding)
+        raw_text_distances.append((name, raw_text_distance))
+        emoji_of_text_distances.append((name, emoji_of_text_distance))
+        image_distances.append((name, image_distance))
+
+    raw_text_distances = sorted(raw_text_distances, key=lambda x: x[1])
+    emoji_of_text_distances = sorted(
+        emoji_of_text_distances, key=lambda x: x[1]
+    )
+    image_distances = sorted(image_distances, key=lambda x: x[1])
+
+    raw_text_kds = raw_text_distances[:top_n]
+    emoji_of_text_kds = emoji_of_text_distances[:top_n]
+    image_kds = image_distances[:top_n]
+    kds = raw_text_kds + emoji_of_text_kds + image_kds
+
+    results = []
+    keys = []
+
+    for kd in kds:
+        name = kd[0]
+        if name in keys:
+            continue
+        keys.append(name)
+        dist = kd[1]
+        unicode = embeddings[name]["unicode"]
+        emoji = embeddings[name]["emoji"]
+        props = {
+            "name": name,
+            "dist": dist,
+            "unicode": unicode,
+            "emoji": emoji,
+        }
+        results.append(props)
+
+    results = sorted(results, key=lambda x: x["dist"])
+    return results[:top_n]
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Search for emojis.")
+    # The number of results is expected to be a separate option
+    parser.add_argument(
+        "--num_results",
+        "-n",
+        type=int,
+        default=5,
+        help="Number of emojis to return",
+    )
+    # The search query will consume all other arguments
+    parser.add_argument(
+        "query", nargs="+", help="The search query for the emoji."
+    )
+
+    args = parser.parse_args()
+
+    # Join all query arguments to form the full search query
+    search_query = " ".join(args.query)
+    num_results = args.num_results
+
+    print(f"Searching for: {search_query}")
+
+    results = search(search_query, top_n=num_results)
+
+    # Truncate distance to 3 decimal places and prepare the table data
+    table_data = [
+        (
+            result["emoji"],
+            result["name"],
+            result["unicode"],
+            f"{result['dist']:.3f}",
+        )
+        for result in results
+    ]
+    print(
+        tabulate(
+            table_data,
+            headers=["Emoji", "Name", "Unicode", "Distance"],
+            tablefmt="pretty",
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
